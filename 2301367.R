8e8c31e4c2257fe8109cd4444d44058e8263c34e
Angel Chang <angel.h.chang@gmail.com>
2023-11-06 18:21:58 +0000

    tidy up

--- 2301367.R
+++ 2301367.R
@@ -1,26 +1,35 @@
-#Programming Asessment
-#Student number 2301367
+# Programming Asessment
+# Student number 2301367
+library(ggplot2)
 
 setwd(dirname(rstudioapi::getSourceEditorContext()$path))
 
 # Read in data from the two files
 births_df <- read.csv("files/births.csv")
-score_df<- read.csv("files/score.csv")
+score_df <- read.csv("files/score.csv")
 
 # Examine the births dataset
 head(births)
 
 # Transform variables to factors with labels
-births_df$lowbw <- factor(births_df$lowbw, levels = c(0, 1),
-                          labels = c("Normal birthweight", "Low birthweight"))
-births_df$preterm <- factor(births_df$preterm, levels = c(0, 1),
-                            labels = c("Not preterm", "Preterm"))
-births_df$hyp <- factor(births_df$hyp, levels = c(0, 1),
-                        labels = c("No maternal hypertension", "Maternal hypertension"))
-births_df$sex <- factor(births_df$sex, levels = c(1, 2),
-                        labels = c("Male", "Female"))
+births_df$lowbw <- factor(births_df$lowbw,
+  levels = c(0, 1),
+  labels = c("Normal birthweight", "Low birthweight")
+)
+births_df$preterm <- factor(births_df$preterm,
+  levels = c(0, 1),
+  labels = c("Not preterm", "Preterm")
+)
+births_df$hyp <- factor(births_df$hyp,
+  levels = c(0, 1),
+  labels = c("No maternal hypertension", "Maternal hypertension")
+)
+births_df$sex <- factor(births_df$sex,
+  levels = c(1, 2),
+  labels = c("Male", "Female")
+)
 
-# Display the first few rows of the data.frame to check the changes. 
+# Display the first few rows of the data.frame to check the changes.
 # Check the transformation into factors as expected.
 head(births_df)
 str(births_df)
@@ -32,10 +41,10 @@ score_df_clean <- na.omit(score_df)
 # Combine the datasets keeping both matching and unmatching records.
 merged_df <- merge(births_df_clean, score_df_clean, by = "id", all = TRUE)
 str(merged_df)
-# This combined dataframe as 490 rows. 
+# This combined dataframe as 490 rows.
 
 # Reorder the merged dataset by the "score" variable.
-sorted_merged_df <- merged_df[order(merged_df$score),]
+sorted_merged_df <- merged_df[order(merged_df$score), ]
 
 
 # Name the continuous variables
@@ -47,7 +56,7 @@ correlation_matrix <- cor(continuous_variables, use = "pairwise.complete.obs")
 # Print the correlation matrix
 print(correlation_matrix)
 
-# Examine two way distriburion of categorical variables as a table. 
+# Examine two way distriburion of categorical variables as a table.
 # Function to print the crosstab and its title
 print_crosstab <- function(data, row_var, col_var, title) {
   cat(title, ":\n")
@@ -56,35 +65,36 @@ print_crosstab <- function(data, row_var, col_var, title) {
 }
 
 # Now call this function for each pair of variables
-print_crosstab(merged_df, "sex", "hyp", 'Sex and Hypertension distribution')
-print_crosstab(merged_df, "sex", "preterm", 'Sex and Preterm distribution')
-print_crosstab(merged_df, "preterm", "hyp", 'Preterm and Hypertension distribution')
-print_crosstab(merged_df, "sex", "lowbw", 'Low birthweight and Sex distribution')
-print_crosstab(merged_df, "lowbw", "hyp", 'Low birthweight and Hypertension distribution')
-print_crosstab(merged_df, "lowbw", "preterm", 'Low birthweight and Preterm distribution')
+print_crosstab(merged_df, "sex", "hyp", "Sex and Hypertension distribution")
+print_crosstab(merged_df, "sex", "preterm", "Sex and Preterm distribution")
+print_crosstab(merged_df, "preterm", "hyp", "Preterm and Hypertension distribution")
+print_crosstab(merged_df, "sex", "lowbw", "Low birthweight and Sex distribution")
+print_crosstab(merged_df, "lowbw", "hyp", "Low birthweight and Hypertension distribution")
+print_crosstab(merged_df, "lowbw", "preterm", "Low birthweight and Preterm distribution")
 
 
-# Create a new variable high score that identifies scores greater than 150. 
- 
- 
+# Create a new variable high score that identifies scores greater than 150.
+
+
 merged_df$highscore <- with(merged_df, ifelse(is.na(score), "No Score",
-                                              ifelse(score > 150, "Yes", "No")))
+  ifelse(score > 150, "Yes", "No")
+))
 
 # Convert the highscore variable to a factor
 merged_df$highscore <- factor(merged_df$highscore, levels = c("No", "Yes", "No Score"))
 
-aggregate(bweight ~ highscore + sex, data=merged_df, FUN=mean)
+aggregate(bweight ~ highscore + sex, data = merged_df, FUN = mean)
 
 
 # Visualize the distribution of birthweight and gestational weeks in a plot
-library(ggplot2)
-
 gestwks_v_bweight <- ggplot(merged_df, aes(x = gestwks, y = bweight)) +
   geom_point(alpha = 0.5) +
   geom_smooth(method = "lm", formula = y ~ x) +
-  labs(x = "Gestational age (weeks)",
-       y = "Birthweight (grams)",
-       title = "Relationship Between Gestational Weeks and Birthweight") +
+  labs(
+    x = "Gestational age (weeks)",
+    y = "Birthweight (grams)",
+    title = "Relationship Between Gestational Weeks and Birthweight"
+  ) +
   theme_bw() +
   theme(plot.title = element_text(hjust = 0.5))
 
@@ -96,4 +106,4 @@ ggsave("BweightvsgestwksR.pdf", plot = p, device = "pdf", width = 7, height = 7)
 
 
 # Save final dataframe
-write.csv(merged_df, file="finaldatasetfromR.csv", row.names=FALSE)
+write.csv(merged_df, file = "finaldatasetfromR.csv", row.names = FALSE)
--- README.md
+++ README.md
@@ -2,16 +2,23 @@
 This folder contains two files, one in Juptyer notebooks and one in R, that merges two datasets in the folder files, cleans and merges them.
 It includes needed files in a folder "files"
 
-## Getting started
+## Getting started - Python
 
-### Dependencies
-### Jupyter Notebooks
+1. Git clone this repository or download as a zip and unzip to local filesystem
+2. `cd programming-assessment`
+3. Create new virtual environment `python -m venv env`
+4. Activate the virtual environment `source env/bin/activate`
+5. Install dependencies `pip install -r requirements.txt`
+5. Run Jupyter Notebook `2301367.ipynb` using this virtual environment
+
+## Getting started - R
 
 ### R file
-Install R version 4.3.1 (2023-06-16) -- "Beagle Scouts"
-Note the working directory has been set to the folder the R file is currently in. 
-This only works in R studio.
-If you are not using R studio, please set working directory to source file location.
+
+1. Install R version 4.3.1 (2023-06-16) -- "Beagle Scouts"
+2. Open `2301367.R` in R Studio
+3. Load depedencies
+4. Run
 
 
 1. One
